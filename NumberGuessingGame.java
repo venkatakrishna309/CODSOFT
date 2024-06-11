c72c45e3db4780ae447f5665e0bceb250deb0b3b
venkatakrishna309 <venkatakrishnamaddineni11@gmail.com>
2024-06-11 15:04:07 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 10;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I've generated a random number between " + minRange + " and " + maxRange + ". Let's play!");
+
+        boolean playAgain = true;
+        while (playAgain) {
+            int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed it right in " + attempts + " attempts.");
+                    score++;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (attempts == maxAttempts) {
+                System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was " + targetNumber + ".");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next().toLowerCase();
+            playAgain = playAgainInput.equals("yes");
+        }
+
+        System.out.println("Thanks for playing! Your total score: " + score);
+        scanner.close();
+    }
+}
